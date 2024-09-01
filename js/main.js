d0feb66f3a91154d7b8876ae0048494546418e0a
wryorange-windows <wryorange@naver.com>
2024-09-01 23:21:52 +0900

    update pay section

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 let tostMessage = document.getElementById('tost_message');
 
 function tostOn(){
-tostMessage.classList.add('active');
+tostMessage.classList.add('activate');
 setTimeout(function(){
-    tostMessage.classList.remove('active');
+    tostMessage.classList.remove('activate');
 },2300);
 }
 
@@ -31,7 +31,6 @@ Kakao.Share.sendDefault({
     imageUrl:
         'https://jiwonjuhong.github.io/images/home.jpg',
     link: {
-        // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
         mobileWebUrl: 'https://jiwonjuhong.github.io/',
         webUrl: 'https://jiwonjuhong.github.io/',
     },
@@ -48,4 +47,21 @@ Kakao.Share.sendDefault({
     },
     ],
 });
+}
+
+// collapsible
+function collapse(element) {
+    // var before = document.getElementsByClassName("active")[0]               // 기존에 활성화된 버튼
+    // if (before && document.getElementsByClassName("active")[0] != element) {  // 자신 이외에 이미 활성화된 버튼이 있으면
+    //     before.nextElementSibling.style.maxHeight = null;   // 기존에 펼쳐진 내용 접고
+    //     before.classList.remove("active");                  // 버튼 비활성화
+    // }
+    element.classList.toggle("active");         // 활성화 여부 toggle
+
+    var content = element.nextElementSibling;
+    if (content.style.maxHeight != 0) {         // 버튼 다음 요소가 펼쳐져 있으면
+        content.style.maxHeight = null;         // 접기
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";  // 접혀있는 경우 펼치기
+    }
 }
